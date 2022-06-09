9c0a98023b5395bf2914905a60f6ad6e78a1b0f2
Guilherme Latrova <hello@guilatrova.dev>
2022-06-09 18:13:51 -0300

    chore: remove unused docstring

--- src/ast_selector/main.py
+++ src/ast_selector/main.py
@@ -10,18 +10,6 @@ from .models import ElementSelector, NavigationReference, SelectorGroup
 
 
 class AstSelector:
-    """
-    CSS(Ast) Selector:
-
-    Raise = instance type
-    > [el] = direct child
-    .abc = ????
-    #abc = ????
-    [attr is X] = any element where attr is X
-    [attr is None] = any element where attr is X
-    [a=c][b=d] = combine attr selectors
-    """
-
     def __init__(self, query: str, tree: ast.AST) -> None:
         self.tree = tree
         self.query = query
